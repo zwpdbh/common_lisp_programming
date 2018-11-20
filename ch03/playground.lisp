c97f05893daf8782bb9d42040a146361a3d5a9be
x1ezmr <frank.zhao@siemens.com>
2018-11-20 13:01:16 +0800

    >ch03 improve the user interaction

---
+++ ch03/playground.lisp
@@ -0,0 +1,12 @@
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+(defvar *db* nil)
+
+(defun add-record (cd) (push cd *db*))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
+;; improving the user interaction
