82fa2cdd0a91ab7ae0085dace10471734766e04b
Zhao, Wei <frank.zhao@siemens.com>
2019-01-08 19:19:50 +0800

    meet error during expand macro: undefined result

---
+++ ch09/playground.lisp
@@ -0,0 +1,82 @@
+;; For instance you wre writing tests for the built-in + function.
+;; simplest thing, you want to know if all cases passed
+(defun test-+ ()
+  (and
+   (= (+ 1 2) 3)
+   (= (+ 1 2 3) 6)
+   (= (+ -1 -3) -4)))
+
+;; you want to know each case's situation
+(defun test-+ ()
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (format t "~:[FAIL~;pass~] ... ~a~%" (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+;; get rid of the repeated similar calls to FORMAT
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+;; now the test-+ become
+(defun test-+ ()
+  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+;; next get rid of the duplication of the test case expression
+;; You;d really like is to be able to treat the expression as both cod (to get the result), and data (to use as the label)
+;; Whenever you want to treat code as data, there is a sure sign you need a macro
+;; (check (= (+ 1 2) 3))
+;; should be equal
+;; (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
+(defmacro check (form)
+  `(report-result ,form ',form))
+(defun test-+ ()
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -3) -4)))
+
+;; why not get rid of those repeated calls to check?
+;; define check to take an arbitary number of forms and wrap them each in a call to report-result
+(defmacro check (&body forms)
+  `(progn
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+(defun test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+
+;; fix the return value, so it returns the result of the test case it is reporting
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
+  result)
+;; Now the report-result returns the result of its test case.
+;; You need an AND without short-circuiting, and use it in the place of progn and you would be done.
+;; You wish
+;; (combine-results
+;;   (foo)
+;;   (bar)
+;;   (baz))
+;; to be equal
+;; (let ((result t))
+;;   (unless (foo) (setf result nil))
+;;   (unless (bar) (setf result nil))
+;;   (unless (baz) (setf result nil))
+;;   result)
+
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defun test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
