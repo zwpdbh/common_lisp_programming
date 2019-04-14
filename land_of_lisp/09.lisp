37b206fe3392873e03dbaa40c422dbbb987e8b7a
Zhao, Wei <frank.zhao@siemens.com>
2019-04-14 15:42:34 +0800

    >

--- land_of_lisp/09.lisp
+++ land_of_lisp/09.lisp
@@ -1,2 +1,74 @@
 ;; THe orc battle game p172
 
+;; track three play status: health, agility and strength
+
+(defparameter *player-health* nil)
+(defparameter *player-agility* nil)
+(defparameter *player-strength* nil)
+
+(defparameter *monsters* nil)
+(defparameter *monster-builders* nil)
+(defparameter *monster-num* 12)
+
+(defun or-battle ()
+  (init-monsters)
+  (init-player)
+  (game-loop)
+  (when (player-dead)
+    (princ "You have been killed. Game Over."))
+  (when (monsters-dead)
+    (printc "Congratulations! You have vanquished all of your foes")))
+
+
+;; Handle the game loop, it handles a round of the battle, and then calls itself recursively for the following round:
+(defun game-loop ()
+  (unless (or (player-dead) (monsters-dead))
+    (show-player)
+    (dotimes (k (1+ (truncate (/ (max 0 *player-agility*) 15))))
+      (unless (monsters-dead)
+	(show-monsters)
+	(player-acctack)))
+    (fresh-line)
+    (map 'list
+	 (lambda (m)
+	   (or (monster-dead m) (monster-attack m)))
+	 *monsters*)
+    (game-loop)))
+
+;; Player Management Functions
+(defun init-player ()
+  (setf *player-health* 30)
+  (setf *player-agility* 30)
+  (setf *player-strength* 30))
+
+(defun player-dead ()
+  (<= *player-health* 0))
+
+(defun show-player ()
+  (fresh-line)
+  (princ "You are a valiant knight with a health of")
+  (printc *player-health*)
+  (princ ", an agilith of ")
+  (princ *player-agility*)
+  (princ ", an a strength of ")
+  (princ *player-strength*))
+
+;; manage a player's attack
+(defun player-attack ()
+  (fresh-line)
+  (princ "Attack style: [s]tab [d]ouble swing [r]oundhouse:")
+  (case (read)
+    (s (monster-hit (pick-monster)
+		    (+ 2 (randval (ash *player-strength* -1)))))
+    (d (let ((x (randval (truncate (/ *player-strength* 6)))))
+	 (princ "You double swing has a strengh of ")
+	 (princ x)
+	 (fresh-line)
+	 (monster-hit (pick-monster) x)
+	 (unless (monsters-dead)
+	   (monster-hit (pick-monster) x))))
+    (otherwise (dotimes (x (1+ (randval (truncate (/ *player-strength* 3)))))
+		 (unless (monsters-dead)
+		   (monster-hit (random-monster) 1))))))
+
+;; 176
