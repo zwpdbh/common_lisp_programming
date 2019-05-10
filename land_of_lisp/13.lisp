20e84417295411d68f7be08d0b3d69a77d988718
Zhao, Wei <frank.zhao@siemens.com>
2019-05-10 17:09:55 +0800

    >

---
+++ land_of_lisp/13.lisp
@@ -0,0 +1,21 @@
+;; from page 259
+;; decode the escape characters from HTTP escape codes
+
+(defun http-char (c1 c2 &optional (default #\space))
+  (let ((code (parse-integer
+	       (coerce (list c1 c2) 'string) :radix 16 :junk-allowed t)))
+    (if code
+	(code-char code)
+	default)))
+
+
+(defun decode-param (s)
+  (labels ((f (lst)
+	     (when lst
+	       (case (car lst)
+		 (#\% (cons (http-char (cadr lst) (caddr lst))
+			    (f (cdddr lst))))
+		 (#\+ (cons #\space (f (cdr lst))))
+		 (otherwise (cons (car lst) (f (cdr lst))))))))
+    (coerce (f (coerce s 'list)) 'string)))
+
